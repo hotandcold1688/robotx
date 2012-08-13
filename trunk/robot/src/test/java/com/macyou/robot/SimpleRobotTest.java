cbab105bc2a2d138fd995a153c308edddfd1d3d7
zldeng1984@gmail.com <zldeng1984@gmail.com@047bcc11-131d-f090-e79c-bd8b5ee7219a>
2012-08-13 01:31:51 +0000

    simplerobot test

--- trunk/robot/src/test/java/com/macyou/robot/SimpleRobotTest.java
+++ trunk/robot/src/test/java/com/macyou/robot/SimpleRobotTest.java
@@ -1,8 +1,10 @@
 package com.macyou.robot;
 
 import java.io.File;
+import java.io.FileReader;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryParser.QueryParser;
@@ -32,11 +34,17 @@ public class SimpleRobotTest {
 
 	static String INDEX_DIR = "target/lucene/index/SimpleRobotTest/";
 	static DefaultIndexBuilderFactory factory = new DefaultIndexBuilderFactory(INDEX_DIR);
+	static Analyzer analyzer;
+	//	static Analyzer analyzer = new IKAnalyzer();
 
-	@BeforeClass //有对象初始化前运行一次的方法么(不要静态的)
+	@BeforeClass
+	//有对象初始化前运行一次的方法么(不要静态的)
 	public static void setUp() throws Exception {
 
-		Analyzer analyzer = new IKAnalyzer(true);
+		analyzer = new IKAnalyzer(true);
+		// analyzer = new IKAnalyzer();
+		//analyzer = new StandardAnalyzer(Constants.LUCENE_VERSION, new FileReader("target/classes/stopword.dic"));
+
 		factory.setAnalyzer(analyzer);
 		JavaFetcher fetcher = new JavaFetcher();
 		fetcher.setSource(TestData.knowledges);
@@ -61,46 +69,53 @@ public class SimpleRobotTest {
 
 	@Test
 	public void testAnswer_samilarity1() throws Exception {
-		Document doc = SearchHelper.searchFirstDoc(INDEX_DIR, "你好吗");
+		Document doc = SearchHelper.searchFirstDoc(INDEX_DIR, "你好吗？");
 		Assert.assertEquals("你好", doc.getFieldable(Knowledge.ANSWER).stringValue());
 	}
 
 	@Test
 	public void testAnswer_samilarity2() throws Exception {
-		Document doc = SearchHelper.searchFirstDoc(INDEX_DIR, "你是?");
+		Document doc = SearchHelper.searchFirstDoc(INDEX_DIR, "你是谁啊？");
 		Assert.assertEquals("我是机器人小邓", doc.getFieldable(Knowledge.ANSWER).stringValue());
 	}
 
 	@Test
 	public void testAnswer_samilarity3() throws Exception {
-		Document doc = SearchHelper.searchFirstDoc(INDEX_DIR, "中国名族有多少");
+		Document doc = SearchHelper.searchFirstDoc(INDEX_DIR, "中国名族多少个");
 		Assert.assertEquals("56个名族", doc.getFieldable(Knowledge.ANSWER).stringValue());
 
 	}
-}
+	
+	@Test
+	public void testAnswer_samilarity4() throws Exception {
+		Document doc = SearchHelper.searchFirstDoc(INDEX_DIR, "宇宙超级无敌美少女是谁");
+		Assert.assertEquals("凤姐", doc.getFieldable(Knowledge.ANSWER).stringValue());
 
-class SearchHelper {
-
-	public static Document searchFirstDoc(String indexDir, String text) throws Exception {
-		Directory directory = FSDirectory.open(new File(indexDir));
-		IndexSearcher searcher = new IndexSearcher(IndexReader.open(directory));
-
-		Analyzer analyzer = new IKAnalyzer(true);
-		QueryParser parser = new QueryParser(Constants.LUCENE_VERSION, Knowledge.QUESTION, analyzer);
-		Query query = parser.parse(text);
-
-		TopDocs docs = searcher.search(query, 1);
-		Document doc = searcher.doc(docs.scoreDocs[0].doc);
-		searcher.close();
-		return doc;
 	}
 
-	//	public static int getSearchHits(String indexDir, String filed, String text) throws CorruptIndexException,
-	//			IOException {
-	//		Directory directory = FSDirectory.open(new File(indexDir));
-	//		IndexSearcher searcher = new IndexSearcher(IndexReader.open(directory));
-	//		Query query = new TermQuery(new Term(filed, text));
-	//		TopDocs docs = searcher.search(query, 2);
-	//		return docs.totalHits;
-	//	}
+	static class SearchHelper {
+
+		public static Document searchFirstDoc(String indexDir, String text) throws Exception {
+			Directory directory = FSDirectory.open(new File(indexDir));
+			IndexSearcher searcher = new IndexSearcher(IndexReader.open(directory));
+
+			//Analyzer analyzer = new IKAnalyzer(true);
+			QueryParser parser = new QueryParser(Constants.LUCENE_VERSION, Knowledge.QUESTION, analyzer);
+			Query query = parser.parse(text);
+
+ 			TopDocs docs = searcher.search(query, 1);
+			Document doc = searcher.doc(docs.scoreDocs[0].doc);
+			searcher.close();
+			return doc;
+		}
+
+		//	public static int getSearchHits(String indexDir, String filed, String text) throws CorruptIndexException,
+		//			IOException {
+		//		Directory directory = FSDirectory.open(new File(indexDir));
+		//		IndexSearcher searcher = new IndexSearcher(IndexReader.open(directory));
+		//		Query query = new TermQuery(new Term(filed, text));
+		//		TopDocs docs = searcher.search(query, 2);
+		//		return docs.totalHits;
+		//	}
+	}
 }
