ff712e447439aa384d60e3f362d014ef8c1f6ad0
zldeng1984@gmail.com <zldeng1984@gmail.com@047bcc11-131d-f090-e79c-bd8b5ee7219a>
2012-08-13 02:14:38 +0000

    add ikananlyzer source

--- trunk/ik-analyzer/install.cmd
+++ trunk/ik-analyzer/install.cmd
@@ -1,1 +1,1 @@
-mvn install:install-file -DgroupId=org.wltea.analyzer -DartifactId=ik-analyzer -Dversion=2012 -Dpackaging=jar -Dfile=IKAnalyzer2012.jar
+mvn install:install-file -DgroupId=org.wltea.analyzer -DartifactId=ik-analyzer -Dversion=2012 -Dpackaging=jar -Dsources=IKAnalyzer2012-sources.jar -Dfile=IKAnalyzer2012.jar 
