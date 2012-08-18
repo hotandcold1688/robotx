dbe8c8c93a0d207ce196d4b71af455e591cdcb74
zldeng1984@gmail.com <zldeng1984@gmail.com@047bcc11-131d-f090-e79c-bd8b5ee7219a>
2012-08-18 02:16:08 +0000

    test put var to vm

--- trunk/robot/src/main/java/com/macyou/web/admin/KnowledgeAdmin.java
+++ trunk/robot/src/main/java/com/macyou/web/admin/KnowledgeAdmin.java
@@ -6,11 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/knowledgeAdmin.htm")
+@RequestMapping("/admin/knowledge")
 public class KnowledgeAdmin {
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/query.htm", method = RequestMethod.GET)
 	public String initForm(ModelMap model) {
-		System.out.println("aaa");
+		model.put("name", "dzl");
+		return "admin/knowledge/query";
+	}
+
+	@RequestMapping(value = "/test2.htm", method = RequestMethod.GET)
+	public String initForm2(ModelMap model) {
+		System.out.println("2");
+		return "admin/knowledgeAdmin";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String handleForm(ModelMap model) {
 		return "admin/knowledgeAdmin";
 	}
 }
