2eb7cccd5e150da087c77f2122ceab351e3d33dd
zldeng1984@gmail.com <zldeng1984@gmail.com@047bcc11-131d-f090-e79c-bd8b5ee7219a>
2012-08-30 04:03:58 +0000

    修改路径

--- trunk/robot/src/main/java/com/macyou/robot/util/WordsSimpleConverter.java
+++ trunk/robot/src/main/java/com/macyou/robot/util/WordsSimpleConverter.java
@@ -1,8 +1,8 @@
 package com.macyou.robot.util;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,7 +36,7 @@ public class WordsSimpleConverter {
 		String line = null;
 		Map<String, String> map = null;
 		try {
-			br = new BufferedReader(new FileReader(url));
+			br = new BufferedReader(new InputStreamReader(WordsSimpleConverter.class.getClassLoader().getResourceAsStream(url)));
 			map = new HashMap<String, String>();
 			while((line = br.readLine()) != null){
 				line = line.trim().toLowerCase();
